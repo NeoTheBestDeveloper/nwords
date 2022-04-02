3a75e8f97bb84b714d5a36524596648c6afc9a43
NeoTheBestDeveloper <neothebestdeveloper@protonmail.com>
2022-04-02 19:27:22 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(name='neowords',
       version='0.1.0',
       packages=['neowords'],
-      entry_points={'console_scripts': ['nw = pycli.__main__:main']})
+      entry_points={'console_scripts': ['neowords = neowords.__main__:main']})
